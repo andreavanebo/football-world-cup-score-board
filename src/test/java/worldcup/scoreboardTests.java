daad013fbb48bcc8b83d8dcf5a2e1352e9eb87ce
Andrea Hetlevik Vanebo <andrehva@stud.ntnu.no>
2024-03-13 13:03:15 +0100

    updated the tests to match the new way of accessing the matches in a scoreboard

--- src/test/java/worldcup/scoreboardTests.java
+++ src/test/java/worldcup/scoreboardTests.java
@@ -29,7 +29,7 @@ public class scoreboardTests {
     public void testStartingScore() {
         assertEquals(2, scoreboard.getNumberOfMatches(), "Matches is not added to the scoreboard correctly.");
 
-        assertEquals(new ArrayList<>(List.of(0, 0)), scoreboard.getMatch(1).getScore(),
+        assertEquals(new ArrayList<>(List.of(0, 0)), scoreboard.getMatch("Spain").getScore(),
                 "The initial score of a match is not 0-0.");
     }
 
@@ -38,7 +38,7 @@ public class scoreboardTests {
         assertEquals(2, scoreboard.getNumberOfMatches(), "Number of matches should be 2.");
         try {
             // home team name is forgotten
-            scoreboard.addMatch(new Match("", "Japan"));
+            Match match = new Match("", "Japan");
             fail("Exception should have been thrown when home team name is empty.");
         } catch (IllegalArgumentException e) {
             assertEquals("Both home team and away team need to have a name.", e.getMessage(),
@@ -50,7 +50,7 @@ public class scoreboardTests {
 
         try {
             // away team name is forgotten
-            scoreboard.addMatch(new Match("England", ""));
+            Match match = new Match("England", "");
             fail("Exception should have been thrown when away team name is empty.");
         } catch (IllegalArgumentException e) {
             assertEquals("Both home team and away team need to have a name.", e.getMessage(),
@@ -62,7 +62,7 @@ public class scoreboardTests {
 
         try {
             // both team names is forgotten
-            scoreboard.addMatch(new Match("", ""));
+            Match match = new Match("", "");
             fail("Exception should have been thrown when both team names is empty.");
         } catch (IllegalArgumentException e) {
             assertEquals("Both home team and away team need to have a name.", e.getMessage(),
@@ -75,42 +75,66 @@ public class scoreboardTests {
     }
 
     @Test
+    public void testAddMatchWithAlreadyPlayingTeam() {
+        // it is allowed to create the matches
+        Match match3 = new Match("Spain", "England");
+        Match match4 = new Match("Norway", "Portugal");
+        Match match5 = new Match("Portugal", "France");
+        // but it should be allowed to add them to the scoreboard
+        this.scoreboard.addMatch(match3);
+        this.scoreboard.addMatch(match4);
+        this.scoreboard.addMatch(match5);
+        assertEquals(2, this.scoreboard.getNumberOfMatches(),
+                "Matches which includes already playing teams shouldn't be added to the scoreboard.");
+    }
+
+    @Test
+    public void testGetMatch() {
+        // trying to get a match which actually exists
+        assertEquals(match2, this.scoreboard.getMatch("Portugal"));
+        assertEquals(match2, this.scoreboard.getMatch("France"));
+
+        // trying to get a match which is not in the scoreboard
+        assertEquals(null, this.scoreboard.getMatch("Norway"));
+    }
+
+    @Test
     public void testUpdateScore() {
-        assertEquals(new ArrayList<>(List.of(0, 0)), scoreboard.getMatch(1).getScore(),
+        assertEquals(new ArrayList<>(List.of(0, 0)), scoreboard.getMatch("Spain").getScore(),
                 "The initial score of a match is not 0-0.");
-        scoreboard.getMatch(1).homeTeamScores();
-        assertEquals(new ArrayList<>(List.of(1, 0)), scoreboard.getMatch(1).getScore(),
+        scoreboard.getMatch("Spain").homeTeamScores();
+        assertEquals(new ArrayList<>(List.of(1, 0)), scoreboard.getMatch("Spain").getScore(),
                 "homeTeamScores() doesn't work as expected.");
-        scoreboard.getMatch(1).homeTeamScores();
-        assertEquals(new ArrayList<>(List.of(2, 0)), scoreboard.getMatch(1).getScore(),
+        scoreboard.getMatch("Spain").homeTeamScores();
+        assertEquals(new ArrayList<>(List.of(2, 0)), scoreboard.getMatch("Spain").getScore(),
                 "homeTeamScores() doesn't work as expected.");
-        scoreboard.getMatch(1).awayTeamScores();
-        assertEquals(new ArrayList<>(List.of(2, 1)), scoreboard.getMatch(1).getScore(),
+        scoreboard.getMatch("Spain").awayTeamScores();
+        assertEquals(new ArrayList<>(List.of(2, 1)), scoreboard.getMatch("Spain").getScore(),
                 "awayTeamScores() doesn't work as expected.");
     }
 
     @Test
     public void testStopMatch() {
         assertEquals(2, scoreboard.getNumberOfMatches(), "The number of matches should be 2 after the setup.");
-        scoreboard.stopMatch(1);
+        scoreboard.stopMatch("Spain");
         assertEquals(1, scoreboard.getNumberOfMatches(), "The match is not removed from the scoreboard.");
-        scoreboard.stopMatch(1);
+        scoreboard.stopMatch("France");
         assertEquals(0, scoreboard.getNumberOfMatches(), "The match is not removed from the scoreboard.");
     }
 
     @Test
     public void testGetScoreBoard() {
         // Spain scores against Denmark
-        scoreboard.getMatch(1).homeTeamScores();
+        scoreboard.getMatch("Spain").homeTeamScores();
         // Denmark scores against Spain
-        scoreboard.getMatch(1).awayTeamScores();
+        scoreboard.getMatch("Spain").awayTeamScores();
         // Denmark scores against Spain
-        scoreboard.getMatch(1).awayTeamScores();
+        scoreboard.getMatch("Spain").awayTeamScores();
 
         // Portugal scores against France
-        scoreboard.getMatch(2).homeTeamScores();
+        scoreboard.getMatch("Portugal").homeTeamScores();
         // France scores against Portugal
-        scoreboard.getMatch(2).awayTeamScores();
+        scoreboard.getMatch("Portugal").awayTeamScores();
 
         /*
          * The scores at this point:
@@ -118,17 +142,17 @@ public class scoreboardTests {
          * Portugal (1) - France (1)
          */
 
-        assertEquals(3, scoreboard.getMatch(1).getScoreSum(), "getScoreSum() doesn't return the correct sum.");
-        assertEquals(2, scoreboard.getMatch(2).getScoreSum(), "getScoreSum() doesn't return the correct sum.");
+        assertEquals(3, scoreboard.getMatch("Spain").getScoreSum(), "getScoreSum() doesn't return the correct sum.");
+        assertEquals(2, scoreboard.getMatch("Portugal").getScoreSum(), "getScoreSum() doesn't return the correct sum.");
 
         assertEquals(2, scoreboard.getScoreBoard().getNumberOfMatches(), "The number of matches should be 2.");
         assertEquals(new ArrayList<>(List.of(match1, match2)),
-                new ArrayList<>(List.of(scoreboard.getScoreBoard().getMatch(1),
-                        scoreboard.getScoreBoard().getMatch(2))),
+                new ArrayList<>(List.of(scoreboard.getScoreBoard().getMatch("Spain"),
+                        scoreboard.getScoreBoard().getMatch("Portugal"))),
                 "The match with the highest total number of goals should be placed first.");
 
         // Portugal scores against France
-        scoreboard.getMatch(2).homeTeamScores();
+        scoreboard.getMatch("Portugal").homeTeamScores();
 
         /*
          * The scores at this point:
@@ -139,12 +163,12 @@ public class scoreboardTests {
         // now it's a tie (both matches have 3 goals), and since Portugal-France is
         // newer it should be placed first in the list
         assertEquals(new ArrayList<>(List.of(match2, match1)),
-                new ArrayList<>(List.of(scoreboard.getScoreBoard().getMatch(1),
-                        scoreboard.getScoreBoard().getMatch(2))),
+                new ArrayList<>(List.of(scoreboard.getScoreBoard().getMatchOnIndex(0),
+                        scoreboard.getScoreBoard().getMatchOnIndex(1))),
                 "When it's a tie, the newest game should be placed first.");
 
         // Spain scores against Denmark
-        scoreboard.getMatch(1).homeTeamScores();
+        scoreboard.getMatch("Spain").homeTeamScores();
 
         /*
          * The scores at this point:
@@ -152,8 +176,8 @@ public class scoreboardTests {
          * Portugal (2) - France (1)
          */
         assertEquals(new ArrayList<>(List.of(match1, match2)),
-                new ArrayList<>(List.of(scoreboard.getScoreBoard().getMatch(1),
-                        scoreboard.getScoreBoard().getMatch(2))),
+                new ArrayList<>(List.of(scoreboard.getScoreBoard().getMatchOnIndex(0),
+                        scoreboard.getScoreBoard().getMatchOnIndex(1))),
                 "The match with the highest total number of goals should be placed first.");
         Match match3 = new Match("Netherlands", "Poland");
         scoreboard.addMatch(match3);
@@ -165,9 +189,9 @@ public class scoreboardTests {
          * Netherlands (0) - Poland (0)
          */
         assertEquals(new ArrayList<>(List.of(match1, match2, match3)),
-                new ArrayList<>(List.of(scoreboard.getScoreBoard().getMatch(1),
-                        scoreboard.getScoreBoard().getMatch(2),
-                        scoreboard.getScoreBoard().getMatch(3))),
+                new ArrayList<>(List.of(scoreboard.getScoreBoard().getMatchOnIndex(0),
+                        scoreboard.getScoreBoard().getMatchOnIndex(1),
+                        scoreboard.getScoreBoard().getMatchOnIndex(2))),
                 "The match with the highest total number of goals should be placed first.");
     }
 
